31c0cd37e26bbd17a9a9a8b68482d4bce61ea041
Arturo Granados <agranados0121@gmail.com>
2022-05-11 19:30:45 -0500

    inital setup

---
+++ ourHotel/src/main/java/team14/ourHotel/OurHotelApplication.java
@@ -0,0 +1,13 @@
+package team14.ourHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurHotelApplication.class, args);
+	}
+
+}
---
+++ ourHotel/src/test/java/team14/ourHotel/OurHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package team14.ourHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
